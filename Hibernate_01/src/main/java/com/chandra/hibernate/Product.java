59b06ade92a54cccc87108fc44c597f3e5abe687
chandra <chandramca04@gmail.com>
2021-01-14 05:42:25 +0530

    first commit

---
+++ Hibernate_01/src/main/java/com/chandra/hibernate/Product.java
@@ -0,0 +1,36 @@
+package com.chandra.hibernate;
+
+public class Product {
+
+	private int pid;
+	private String pname;
+	private int qty;
+	private double price;
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
