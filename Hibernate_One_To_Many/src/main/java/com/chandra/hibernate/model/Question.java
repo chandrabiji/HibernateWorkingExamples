e2a760e5a13f60dad617df5cb8583fa5cb26f5f3
chandra <chandramca04@gmail.com>
2021-01-15 06:30:49 +0530

    Hibernate One To Many

---
+++ Hibernate_One_To_Many/src/main/java/com/chandra/hibernate/model/Question.java
@@ -0,0 +1,52 @@
+package com.chandra.hibernate.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="QUESTIONS")
+public class Question {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	@Column(name="Q_NAME")
+	private String qname;
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="QID")
+	@OrderColumn(name="type")
+	private List<Answer> answers;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getQname() {
+		return qname;
+	}
+	public void setQname(String qname) {
+		this.qname = qname;
+	}
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+	
+	
+
+}
